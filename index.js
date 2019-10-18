fd19c38aef83913a62815d434fea99fc418a2669
Bilal Saeed <bilalsaeed232@gmail.com>
2019-10-18 21:52:12 +0200

    DONE: basic example

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // main entry file of the project
+
+const TodoStore = require("./src/todostore.class");
+
+const todoStore = new TodoStore();
+
+// note we are always calling report
+// function to see the current state after every change (mobx comes into play here, see next branch)
+
+todoStore.addTodo("Take Shower");
+console.log(todoStore.report());
+
+todoStore.addTodo("Eat Breakfast");
+console.log(todoStore.report());
+
+todoStore.todos[0].completed = true;
+todoStore.addTodo("Workout");
+console.log(todoStore.report());
+
+todoStore.todos[1].task = "Eat heavy breakfast!";
+console.log(todoStore.report());
+
+todoStore.todos[0].task = "Take cold shower";
+console.log(todoStore.report());
---
+++ src/todostore.class.js
@@ -0,0 +1,29 @@
+class TodoStore {
+  constructor() {
+    this.todos = [];
+  }
+
+  get completedTodosCount() {
+    return this.todos.filter(todo => todo.completed === true).length;
+  }
+
+  report() {
+    if (this.todos.length === 0) {
+      return "<none>";
+    }
+    return (
+      `Next Todo: "${this.todos[0].task}". ` +
+      `Progress: ${this.completedTodosCount}/${this.todos.length}`
+    );
+  }
+
+  addTodo(task) {
+    this.todos.push({
+      task,
+      completed: false,
+      assignee: null
+    });
+  }
+}
+
+module.exports = TodoStore;
